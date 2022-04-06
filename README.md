c879490bcbb6cf1c80f297e3e6d46123eaeb1c91
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-04-07 00:44:54 +0100

    Initial functionality

---
+++ README.md
@@ -0,0 +1,9 @@
+# Very rough copyright year checker
+
+
+```yaml
+-   repo: git://github.com/benjeffery/pre-commit-copyright-year
+    rev: v1
+    hooks:
+    -   id: copyright-year
+```
---
+++ pre_commit_hooks/copyright_year.py
@@ -0,0 +1,28 @@
+import sys
+import subprocess
+import datetime
+from itertools import islice
+
+def main():
+    for filename in sys.argv[1:]:
+        result = subprocess.run(["git","status",filename], capture_output=True, text=True)
+        try:
+            if "Changes to be committed" in result.stdout:
+                year = datetime.datetime.now().year
+            elif "nothing to commit" in result.stdout:
+                year = subprocess.run(['git', 'log', '-1', '--format="%as"', '--', filename], capture_output=True, text=True).stdout[1:5]
+            else:
+                continue
+            with open(filename, "r") as f:
+                for line in islice(f.readlines(),4):
+                    if "Copyright" in line:
+                        if str(year) in line:
+                            break
+                        else:
+                            print(filename, 'has incorrect copyright year of "' , line.strip(), '"')
+                            return 1
+
+        except:
+            print("Error in year checking - skipping")
+            pass
+    return 0
